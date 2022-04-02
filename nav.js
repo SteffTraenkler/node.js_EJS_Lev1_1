9cff4d5a00eadf8f3d434a06c85696330b33ad7b
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-2.local>
2022-04-02 17:33:55 +0200

    first ejs project

---
+++ nav.js
@@ -0,0 +1,22 @@
+const nav = [
+    {
+        name: "Home",
+        url: "/"
+    },
+    {
+        name: "Team",
+        url: "/team"
+    },
+    {
+        name: "About",
+        url: "/about"
+    },
+    {
+        name: "Contact",
+        url: "/contact"
+    }
+]
+
+module.exports = {
+    nav
+}
